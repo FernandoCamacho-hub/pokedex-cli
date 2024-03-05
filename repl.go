1e44288010295b60c721d8fd2c9e2698a39fac88
Fernando Camacho <salvadorfernando2002@outlook.com>
2024-03-05 18:39:00 +0100

    add repl cycle and cleaning input

---
+++ repl.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startREPL() {
+    scanner := bufio.NewScanner(os.Stdin)
+    for {
+        fmt.Print(" > ")
+
+        scanner.Scan()
+        text := scanner.Text()
+        cleaned := cleanInput(text)
+
+        fmt.Println("echoing:", cleaned)
+    }
+
+}
+
+func cleanInput(str string) []string {
+    lowered := strings.ToLower(str)
+    words := strings.Fields(lowered)
+    return words
+}
